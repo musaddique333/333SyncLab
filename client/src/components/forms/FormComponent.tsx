9806252785ca907e43c21c3cb21e945277ed4b12
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2024-08-24 06:01:27 +0100

    updated the forum component

--- client/src/components/forms/FormComponent.tsx
+++ client/src/components/forms/FormComponent.tsx
@@ -2,23 +2,31 @@ import { useAppContext } from "@/context/AppContext";
 import { useSocket } from "@/context/SocketContext";
 import { SocketEvent } from "@/types/socket";
 import { USER_STATUS } from "@/types/user";
-import { ChangeEvent, FormEvent, useEffect, useRef } from "react";
-import { toast } from "react-hot-toast";
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import logo from "@/assets/logo.svg";
+import toast from 'react-hot-toast';
 
 const FormComponent = () => {
     const location = useLocation();
     const { currentUser, setCurrentUser, status, setStatus } = useAppContext();
     const { socket } = useSocket();
-
     const usernameRef = useRef<HTMLInputElement | null>(null);
     const navigate = useNavigate();
+    const [usernameEntered, setUsernameEntered] = useState(false);
 
     const createNewRoomId = () => {
+        if (!usernameEntered) {
+            toast.error('Please enter your username first');
+            return;
+        }
+        if (currentUser.username.length < 3) {
+            toast.error('Username must be at least 3 characters long');
+            return;
+        }
         setCurrentUser({ ...currentUser, roomId: uuidv4() });
-        toast.success("Created a new Room ID");
+        toast.success('Created a new Room ID');
         usernameRef.current?.focus();
     };
 
@@ -26,20 +34,26 @@ const FormComponent = () => {
         const name = e.target.name;
         const value = e.target.value;
         setCurrentUser({ ...currentUser, [name]: value });
+        if (name === "username") {
+            setUsernameEntered(value.length > 0);
+        }
     };
 
     const validateForm = () => {
+        if (currentUser.username.length < 3) {
+            toast.error('Username must be at least 3 characters long');
+            return false;
+        }
+        if (currentUser.roomId.length < 5) {
+            toast.error('Room ID must be at least 5 characters long');
+            return false;
+        }
         if (currentUser.username.length === 0) {
-            toast.error("Enter your username");
+            toast.error('Enter your username');
             return false;
-        } else if (currentUser.roomId.length === 0) {
-            toast.error("Enter a room ID");
-            return false;
-        } else if (currentUser.roomId.length < 5) {
-            toast.error("Room ID must be at least 5 characters long");
-            return false;
-        } else if (currentUser.username.length < 3) {
-            toast.error("Username must be at least 3 characters long");
+        }
+        if (currentUser.roomId.length === 0) {
+            toast.error('Enter a room ID');
             return false;
         }
         return true;
@@ -49,12 +63,12 @@ const FormComponent = () => {
         e.preventDefault();
         if (status === USER_STATUS.ATTEMPTING_JOIN) return;
         if (!validateForm()) return;
-        toast.loading("Joining room...");
+        toast.loading('Joining room...');
         setStatus(USER_STATUS.ATTEMPTING_JOIN);
         socket.emit(SocketEvent.JOIN_REQUEST, currentUser);
     };
 
-    useEffect(() => {
+useEffect(() => {
         if (currentUser.roomId.length > 0) return;
         if (location.state?.roomId) {
             setCurrentUser({ ...currentUser, roomId: location.state.roomId });
@@ -89,17 +103,9 @@ const FormComponent = () => {
     }, [currentUser, location.state?.redirect, navigate, setStatus, socket, status]);
 
     return (
-        <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-6 p-6 bg-gradient-to-b from-blue-700 from-80% to-pink-500 from-20% rounded-lg shadow-lg">
+        <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-6 p-6 bg-gradient-to-br from-blue-500 via-purple-600 to-pink-500 rounded-lg shadow-lg">
             <img src={logo} alt="Logo" className="w-92 mb-6" />
             <form onSubmit={joinRoom} className="flex w-full flex-col gap-4">
-                <input
-                    type="text"
-                    name="roomId"
-                    placeholder="Room ID"
-                    className="w-full rounded-md border border-gray-600 bg-gray-800 text-white px-4 py-3 focus:outline-none focus:ring-2 focus:ring-pink-300 transition-all duration-300"
-                    onChange={handleInputChanges}
-                    value={currentUser.roomId}
-                />
                 <input
                     type="text"
                     name="username"
@@ -109,6 +115,14 @@ const FormComponent = () => {
                     value={currentUser.username}
                     ref={usernameRef}
                 />
+                <input
+                    type="text"
+                    name="roomId"
+                    placeholder="Room ID"
+                    className="w-full rounded-md border border-gray-600 bg-gray-800 text-white px-4 py-3 focus:outline-none focus:ring-2 focus:ring-pink-300 transition-all duration-300"
+                    onChange={handleInputChanges}
+                    value={currentUser.roomId}
+                />
                 <button
                     type="submit"
                     className="mt-4 w-full rounded-md bg-pink-500 hover:bg-pink-600 px-8 py-3 text-lg font-semibold text-white transition-all duration-300"
@@ -117,20 +131,20 @@ const FormComponent = () => {
                 </button>
             </form>
             <a
-            href="#"
-            onClick={createNewRoomId}
-            className="bg-[#1D4ED8] group relative inline-flex items-center overflow-hidden rounded-full px-8 py-3 transition"
+                href="#"
+                onClick={createNewRoomId}
+                className="bg-[#1D4ED8] group relative inline-flex items-center overflow-hidden rounded-full px-8 py-3 transition"
             >
-            <div className="absolute inset-0 flex items-center">
-                <div className="absolute h-full w-full animate-spin bg-[conic-gradient(from_0_at_50%_50%,rgba(255,192,203,0.5)_0deg,transparent_60deg,transparent_300deg,rgba(255,192,203,0.5)_360deg)] opacity-0 transition duration-300 group-hover:opacity-100"></div>
-            </div>
-            <div className="absolute inset-0.5 rounded-full bg-black/90"></div>
-            <div className="absolute bottom-0 left-1/2 h-1/3 w-4/5 -translate-x-1/2 rounded-full bg-pink-400/10 opacity-70 blur-md transition-all duration-500 group-hover:h-2/3 group-hover:opacity-100"></div>
-            <div className="relative inline-flex items-center gap-2">
-                <span className="font-mona mt-px bg-gradient-to-b from-pink-200/75 to-pink-400 bg-clip-text text-sm font-medium text-transparent transition-all duration-200">
-                Generate Unique Room ID
-                </span>
-            </div>
+                <div className="absolute inset-0 flex items-center">
+                    <div className="absolute h-full w-full animate-spin bg-[conic-gradient(from_0_at_50%_50%,rgba(255,192,203,0.5)_0deg,transparent_60deg,transparent_300deg,rgba(255,192,203,0.5)_360deg)] opacity-0 transition duration-300 group-hover:opacity-100"></div>
+                </div>
+                <div className="absolute inset-0.5 rounded-full bg-black/90"></div>
+                <div className="absolute bottom-0 left-1/2 h-1/3 w-4/5 -translate-x-1/2 rounded-full bg-pink-400/10 opacity-70 blur-md transition-all duration-500 group-hover:h-2/3 group-hover:opacity-100"></div>
+                <div className="relative inline-flex items-center gap-2">
+                    <span className="font-mona mt-px bg-gradient-to-b from-pink-200/75 to-pink-400 bg-clip-text text-sm font-medium text-transparent transition-all duration-200">
+                        Generate Unique Room ID
+                    </span>
+                </div>
             </a>
         </div>
     );
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
-import illustration from "@/assets/illustration.svg"
-import FormComponent from "@/components/forms/FormComponent"
+import illustration from "@/assets/illustration.svg";
+import FormComponent from "@/components/forms/FormComponent";
 // import Footer from "@/components/common/Footer";
 
 function HomePage() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center gap-16">
+        <div className="flex min-h-screen flex-col items-center justify-center gap-16 relative">
+            <div className="absolute inset-0 -z-10 bg-gradient-to-br from-blue-400 via-purple-500 to-pink-400 opacity-70"></div>
             <div className="my-12 flex h-full min-w-full flex-col items-center justify-evenly sm:flex-row sm:pt-0">
                 <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
                     <img
@@ -19,7 +20,7 @@ function HomePage() {
             </div>
             {/* <Footer /> */}
         </div>
-    )
+    );
 }
 
-export default HomePage
+export default HomePage;
